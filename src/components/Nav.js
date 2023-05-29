c10547533b54bd1cb1b62d8b6732b04a7a3f5cc3
Cole <ce.kasabian@gmail.com>
2023-05-29 15:37:42 -0700

    completed the myWork page

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,31 +1,31 @@
 import "../App.css";
-import React from "react";
+import React, { useRef } from "react";
+import Resume from "../Assets/cole-kasabian-resume.docx";
 
 export default function Nav({ currentPage, handlePageChange }) {
   return (
     <div>
       <div className="mobileContainer">
         <nav className="nav-bar">
-          <a onClick={() => handlePageChange("Home")} >
-            <h1> CK</h1>
-          </a>
+          <div className="homeBtn">
+            <a onClick={() => handlePageChange("Home")}>
+              <h1> CK</h1>
+            </a>
+            <form method="get" className="myResume" action={Resume}>
+              <button type="submit" className="homeResume">
+                Resume`
+              </button>
+            </form>
+          </div>
           <ul className="nav-links">
             <li className="tabs">
-              <a onClick={() => handlePageChange("AboutMe")} >
-                About Me
-              </a>
+              <a onClick={() => handlePageChange("AboutMe")}>About Me</a>
             </li>
             <li className="tabs">
-              <a onClick={() => handlePageChange("MyWork")}>
-                My Work
-              </a>
+              <a onClick={() => handlePageChange("MyWork")}>My Work</a>
             </li>
             <li className="tabs">
-              <a
-                onClick={() => handlePageChange("ContactMe")}
-              >
-                Contact Me
-              </a>
+              <a onClick={() => handlePageChange("ContactMe")}>Contact Me</a>
             </li>
           </ul>
         </nav>
